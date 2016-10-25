eb72ad6dde1c712dfea7bb2de10615fce27c7249
Girlene <girlene.5050@hotmail.com>
2016-10-25 19:46:10 -0300

    Iniciado projeto

---
+++ Projeto_poo/src/main/java/com/example/ProjetoPooApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPooApplication.class, args);
+	}
+}
