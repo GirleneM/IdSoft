1bc10acac3ffe594c828920adfe56e944ab99a12
Flamael <flamaell@hotmail.com>
2016-11-21 17:56:17 -0300

    reenvio denovo

--- Projeto_poo/src/main/java/com/javalingo/view/Sobre.java
+++ Projeto_poo/src/main/java/com/javalingo/view/Sobre.java
@@ -43,7 +43,6 @@ public class Sobre extends JFrame {
 	 * Create the frame.
 	 */
 	public Sobre() {
-		
 		setTitle("Javalingo - Sobre");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
